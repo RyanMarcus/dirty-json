e6258bf03b9e7bfdd36ae7fa144141ee377b0038
Ryan Marcus <ryan@rmarcus.info>
2014-10-04 16:39:25 -0400

    Added licence

--- test/parser_test.js
+++ test/parser_test.js
@@ -1,3 +1,20 @@
+// Copyright 2014 Ryan Marcus
+// This file is part of dirty-json.
+// 
+// dirty-json is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// dirty-json is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with dirty-json.  If not, see <http://www.gnu.org/licenses/>.
+
+
 var assert = require("assert");
 var parser = require("../parser");
 
