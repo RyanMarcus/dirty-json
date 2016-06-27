f715bd6c5ac43bee122cb53b2e59e70cb613c864
Ryan Marcus <ryan@rmarcus.info>
2016-06-27 18:45:02 -0400

    added strict

--- parser.js
+++ parser.js
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with dirty-json.  If not, see <http://www.gnu.org/licenses/>.
 
+"use strict";
+
 var fs = require('fs');
 var Stream = require('stream');
 var lexer = require("./lexer");
