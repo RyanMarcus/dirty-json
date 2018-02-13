0b9c15e26699538a11e08e0f168af558392dadbe
Ryan Marcus <ryan@rmarcus.info>
2018-02-12 21:57:40 -0500

    (probably) fixed REDOS issues

--- lexer.js
+++ lexer.js
@@ -76,44 +76,44 @@ function parseString(str) {
 
 function getLexer(string) {
     let lexer = new Lexer();
-    lexer.addRule(/"((?:[^"\\]+|\\.)*)("|$)/, (lexeme, txt) => {
-	return {type: LEX_QUOTE, value: parseString(txt)};
+    lexer.addRule(/"((?:\\.|[^"])*)($|")/, (lexeme, txt) => {
+        return {type: LEX_QUOTE, value: parseString(txt)};
     });
 
-    lexer.addRule(/'((?:[^'\\]+|\\.)*)('|$)/, (lexeme, txt) => {
-	return {type: LEX_QUOTE, value: parseString(txt)};
+    lexer.addRule(/'((?:\\.|[^'])*)($|')/, (lexeme, txt) => {
+        return {type: LEX_QUOTE, value: parseString(txt)};
     });
 
     // floats with a dot
     lexer.addRule(/[\-0-9]*\.[0-9]*([eE][\+\-]?)?[0-9]*/, lexeme => {
-	return {type: LEX_FLOAT, value: parseFloat(lexeme)};
+        return {type: LEX_FLOAT, value: parseFloat(lexeme)};
     });
 
     // floats without a dot but with e notation
     lexer.addRule(/\-?[0-9]+([eE][\+\-]?)[0-9]*/, lexeme => {
-	return {type: LEX_FLOAT, value: parseFloat(lexeme)};
+        return {type: LEX_FLOAT, value: parseFloat(lexeme)};
     });
     
     lexer.addRule(/[\-0-9]+/, lexeme => {
-	return {type: LEX_INT, value: parseInt(lexeme)};
+        return {type: LEX_INT, value: parseInt(lexeme)};
     });
 
     lexSpc.forEach(item => {
-	lexer.addRule(item[0], lexeme => {
-	    return {type: item[1], value: lexeme};
-	});
+        lexer.addRule(item[0], lexeme => {
+            return {type: item[1], value: lexeme};
+        });
     });
 
     lexer.addRule(/\s/, lexeme => {
-	// chomp whitespace...
+        // chomp whitespace...
     });
 
     lexer.addRule(/./, lexeme => {
-	let lt = LEX_TOKEN;
-	let val = lexeme;
+        let lt = LEX_TOKEN;
+        let val = lexeme;
 
-	
-	return {type: lt, value: val};
+
+        return {type: lt, value: val};
     });
 
     lexer.setInput(string);
@@ -129,7 +129,7 @@ function lexString(str, emit) {
 
     let token = "";
     while ((token = lex.lex())) {
-	emit(token);
+        emit(token);
     }
     
 }
@@ -140,7 +140,7 @@ function getAllTokens(str) {
 
     let arr = [];
     let emit = function (i) {
-	arr.push(i);
+        arr.push(i);
     };
 
     lexString(str, emit);
@@ -149,5 +149,7 @@ function getAllTokens(str) {
     return toR.promise;
 }
 
+console.log("\"this is a \\\" test\"");
+getAllTokens("\"this is a \\\" test\"").then(console.log);
 
 
