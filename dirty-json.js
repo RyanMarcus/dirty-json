3c7a03d3b5eee65663427e0339b7accb07c496ea
Ryan Marcus <ryan@rmarcus.info>
2018-03-30 16:43:38 -0400

    f

--- dirty-json.js
+++ dirty-json.js
@@ -1,22 +1,21 @@
 // < begin copyright > 
 // Copyright Ryan Marcus 2018
-//
-// This file is part of dirty-json.
 // 
-// dirty-json is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 // 
-// dirty-json is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 // 
-// You should have received a copy of the GNU General Public License
-// along with dirty-json.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 // 
 // < end copyright > 
+ 
 
 
 "use strict";
