b1d67c660119cb587687115b37fde7d81395417d
Ryan Marcus <ryan@rmarcus.info>
2018-08-21 14:16:04 -0400

    fixed script

--- DemoCtrl.js
+++ DemoCtrl.js
@@ -2,48 +2,41 @@ var dJSON = require('dirty-json');
 
 
 angular.module('djson', []).controller('DemoCtrl', ['$scope', function($scope) {
-	$scope.valid = false;
-	
+    $scope.valid = false;
+
+    $scope.examples = [   
+        {"name": "Invalid JSON: embedded HTML", "content": '{ "key": "<div class="coolCSS">some text</div>" }'},
+        {"name": "Valid JSON: Simple", "content": '{ "key": "value" }'},
+        {"name": "Invalid JSON: Simple", "content": "{ key: 'value' }"},
+        {"name": "Valid JSON: Complex Object", "content": '{ "key": ["value", 0.5, \n\t{ "test": 56, \n\t"test2": [true, null] }\n\t]\n}'},
+        {"name": "Invalid JSON: Complex Object", "content": '{ key: ["value", .5, \n\t{ "test": 56, \n\t\'test2\': [true, null] }\n\t]\n}'},
+        {"name": "Invalid JSON: With newlines", "content": '{ "key": "a string\nwith a newline" }'},
+        {"name": "Invalid JSON: Floats", "content": '{ "no leading zero": .13452 }'},
+        {"name": "Invalid JSON: Non-quoted keys", "content": '{ "test": here, "another": test }'}
+    ];
+    
 
 
-	$scope.examples = [			   
-		{"name": "Invalid JSON: embedded HTML", "content": '{ "key": "<div class="coolCSS">some text</div>" }'},
-		{"name": "Valid JSON: Simple", "content": '{ "key": "value" }'},
-		{"name": "Invalid JSON: Simple", "content": "{ key: 'value' }"},
-		{"name": "Valid JSON: Complex Object", "content": '{ "key": ["value", 0.5, \n\t{ "test": 56, \n\t"test2": [true, null] }\n\t]\n}'},
-		{"name": "Invalid JSON: Complex Object", "content": '{ key: ["value", .5, \n\t{ "test": 56, \n\t\'test2\': [true, null] }\n\t]\n}'},
-		{"name": "Invalid JSON: With newlines", "content": '{ "key": "a string\nwith a newline" }'},
-		{"name": "Invalid JSON: Floats", "content": '{ "no leading zero": .13452 }'},
-		{"name": "Invalid JSON: Non-quoted keys", "content": '{ "test": here, "another": test }'}
-	];
-	
+    $scope.selectedExample = $scope.examples[0];
 
+    $scope.input = $scope.selectedExample.content;
 
-	$scope.selectedExample = $scope.examples[0];
+    $scope.doParse = function() {
+        var res = dJSON.parse($scope.input);
+        $scope.output = JSON.stringify(res, null, 4);
+        try {
+            JSON.parse($scope.input);
+            $scope.valid = true;
+        } catch (e) {
+            $scope.valid = false;
+        }
+    };
 
-	$scope.input = $scope.selectedExample.content;
+    
+    $scope.doSelection = function () {
+        $scope.input = $scope.selectedExample.content;
+        $scope.doParse();
+    };
 
-	$scope.doParse = function() {
-		dJSON.parse($scope.input).then(function (res) {
-			$scope.output = JSON.stringify(res, null, 4);
-			try {
-				JSON.parse($scope.input);
-				$scope.valid = true;
-			} catch (e) {
-				$scope.valid = false;
-			}
-
-			$scope.$apply();
-		}).catch(function (e) {
-			alert("Error: " + e);
-		});
-	};
-
-	
-	$scope.doSelection = function () {
-		$scope.input = $scope.selectedExample.content;
-		$scope.doParse();
-	};
-
-	$scope.doParse();
+    $scope.doParse();
 }]);
